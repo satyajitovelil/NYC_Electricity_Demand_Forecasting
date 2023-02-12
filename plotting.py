656643d097950f4a659e00ce6deed189f8b8bab8
satyajit.ovelil <satyajit.ovelil@climate-connect.com>
2023-02-12 20:51:43 +0530

    feat: plotting module

--- plotting.py
+++ plotting.py
@@ -1,4 +1,6 @@
 import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def create_sns_plot(df, x, y, col, hue, units):
     
@@ -54,3 +56,42 @@ def create_sns_plot(df, x, y, col, hue, units):
     g.set_titles("")
     g.set_axis_labels("", "Demand")
     g.tight_layout()
+
+class plotwindow:
+    '''
+    Helps visualise a time window of seasonal and residual components by iterating over a date range. 
+    '''
+
+    def __init__(self, decomp, freq):
+        '''
+        Init Function. 
+        Args:
+            decomp: the result returned by statsmodels seasonal decompose function.
+            freq: the date range to iterate over. 
+        Returns:
+            Initializes the class for iterative plotting plot of seasonal and residual components.
+        '''
+        self.get_date_range(decomp, freq)
+        self.right_dt_bound = self.date_range[1:]
+        self.left_dt_bound = self.date_range[:-1]
+        self.strafe = zip(self.left_dt_bound, self.right_dt_bound)
+        self.seasonal_df = pd.DataFrame(decomp.seasonal)
+        self.residual_df = pd.DataFrame(decomp.resid)
+
+    def get_date_range(self, decomp, freq):
+        st = decomp.observed.index.min()
+        en = decomp.observed.index.max()
+        self.date_range = pd.date_range(start=st, end=en, freq=freq)
+
+    def plot(self):
+        '''
+        Plots the Components. 
+        Args:
+            self: Takes args initialized by the Class.
+        Returns:
+            plots the next window of seasonal and residual components.
+        '''
+        next_window = next(self.strafe)
+        ax = self.seasonal_df.loc[next_window[0]:next_window[1], :].plot(sharey=False)
+        self.residual_df.loc[next_window[0]:next_window[1], :].plot(ax=ax, sharey=False)
+        plt.show()
